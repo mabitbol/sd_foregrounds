5b3e311cb75b93ba69861775f914d69cf5e67cc6
Colin Hill <jcolin.hill@gmail.com>
2016-08-10 12:57:14 -0400

    minor edits -JCH

--- foregrounds.py
+++ foregrounds.py
@@ -29,18 +29,19 @@ def dbdt(nu, T):
 
 
 ### Foreground components from PlanckX2015 ###
+# see Table 4 of https://arxiv.org/pdf/1502.01588v2.pdf
 # Here we are in brightness tempearture (as a first pass) with unit K Rayleigh Jeans
 # I've put the best fit Planck values as defaults
 
 # Thermal Dust
-# Params Ad, Bd, Td which are amplitude, index, and temperature
+# Params Ad, Bd, Td which are amplitude [K_RJ, brightness temp fluctuation w.r.t. CMB blackbody], spectral index, and temperature [K]
 def thermal_dust(nu, Ad=163.0e-6, Bd=1.51, Td=21.0):
     nu0 = 545.0e9   #planck frequency
     gam = hplanck/(kboltz*Td)   
     return Ad * (nu/nu0)**(Bd+1.0) * (np.exp(gam*nu0) - 1.0) / (np.exp(gam*nu) - 1.0)
 
 # Synchrotron (based on Haslam and GALPROP) 
-# Params As, alpha : amplitude and shift parameter
+# Params As, alpha : amplitude [K_RJ, brightness temp fluctuation w.r.t. CMB blackbody] and shift parameter
 def synchrotron(nu, As=20.0, alpha=0.26):
     #for details use synch_temp.info and synch_temp[2].columns 
     # frequency is in GHz in the file and ranges from 1 MHz to 100 THz
@@ -58,16 +59,16 @@ def synchrotron(nu, As=20.0, alpha=0.26):
     return As * (nu0/nu)**2 * numer_fs / denom_fs
 
 # Free-free 
-# Params EM, Te : emission measure (=integrated square electron density along LOS) and electron temp
+# Params EM, Te : emission measure (=integrated square electron density along LOS) and electron temp [K]
 def freefree(nu, EM=13.0, Te=7000.0):
     T4 = (Te * 10**-4)**(-3./2.)
     f9 = nu / (10**9)
-    gff = np.log10(np.exp(5.960 - (np.sqrt(3.)/np.pi) * np.log10(f9*T4)) + np.e)
+    gff = np.log10(np.exp(5.960 - (np.sqrt(3.)/np.pi) * np.log10(f9*T4)) + np.e) #JCH: should this be natural log instead of log_10? the paper doesn't say explicitly...
     tau = 0.05468 * (Te**(-3./2.)) * EM * gff / f9**2
     return (1.0 - np.exp(-tau)) * Te
 
 # AME
-# Params Asd, fp : amplitude and peak frequency
+# Params Asd, fp : amplitude [K_RJ, brightness temp fluctuation w.r.t. CMB blackbody] and peak frequency
 def ame(nu, Asd=92.0e-6, nup=19.0e9):
     # template nu go from 50 MHz to 500 GHz...
     # had to add a fill value of 1.e-6 at high frequencies...
@@ -85,21 +86,20 @@ def ame(nu, Asd=92.0e-6, nup=19.0e9):
     
 # SZ
 # params Asz>0
-# including this as a check but shouldnt it be the same as the y distortion 
+# including this as a check but is of course identical to y-distortion
 def sz(nu, ysz=1.4e-6):
     X = hplanck*nu/(kboltz*TCMB)
     gf = (np.exp(X)-1)**2 / (X*X*np.exp(X))
-    return (ysz*10**6)*TCMB * ( (X*np.exp(X)+1.)/(np.exp(X)-1.) - 4.) / gf
-
+    return ysz*TCMB * ( X*(np.exp(X)+1.)/(np.exp(X)-1.) - 4.) / gf #JCH: fixed some errors here
 
 # Line emission
 # this needs more work. should look in paper about CO emission as spectral distortion
 
+# CIB emission
+# need to implement
 
 # CMB rms in brightness temp
 def cmb(freqs, T=TCMB, A=3.0e-6):
     X = hplanck*freqs/(kboltz*T)
     gf = (np.exp(X)-1)**2 / (X*X*np.exp(X))
     return A/gf
-    
-
