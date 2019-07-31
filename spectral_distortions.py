83102d0430af6bba03c71a6a3cf801f0bf41b97f
Maximilian H Abitbol <mabitbol15@gmail.com>
2019-07-31 15:36:06 +0100

    tcmb

--- spectral_distortions.py
+++ spectral_distortions.py
@@ -2,7 +2,7 @@ import numpy as np
 
 ### See components for a better description of the signals. 
 
-TCMB = 2.725 #Kelvin
+TCMB = 2.7255 #Kelvin
 hplanck = 6.626070150e-34  # MKS
 kboltz = 1.380649e-23  # MKS
 clight=299792458.0 #MKS
